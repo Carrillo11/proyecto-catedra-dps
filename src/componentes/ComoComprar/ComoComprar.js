aa25448ddbea925600921dad4015a74d8b1f0387
Manu999hdez <HL180507@alumno.udb.edu.sv>
2020-10-18 00:32:24 -0600

    Agregando Como comprar y quienes somos con Rene Carrillo

--- src/componentes/ComoComprar/ComoComprar.js
+++ src/componentes/ComoComprar/ComoComprar.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react';
 
+//Importamos la imagen de la pagina
+import imagen from '../elementos/img/como-comprar.jpg';
+import imagen1 from '../elementos/img/paso1.PNG';
+
 class ComoComprar extends Component {
     render() { 
         return (
-            <div className="ComoComprar">
-                <h1>Componente como comprar</h1>
+            <div class="ComoComprar">
+                <div class="contenedor-info">
+                    <img src={imagen} alt="Imagen registro " />
+                    <div class="info">
+                        <h2>¿Como comprar?</h2>
+                        <p>Ahora hacer tus pedidos esta a un solo click de distancia </p>
+                    </div>
+                </div>
+
+                <h1>PROCESO DE COMPRA</h1>
+            <div class="descrip"> 
+                <h3>Para poder comprar sin problema los productos de nuestra tienda, sigue los pasos a continuacion</h3>
+                  
+            </div>
+            <div class="grupos-pasos">
+                <div class="pasos">
+                <h3>PASO 1:</h3>
+                <img src={imagen1} alt="" />
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, aliquid dolorem obcaecati itaque ad tenetur vero impedit accusantium aut modi atque eum quod, ducimus eos numquam error iusto, quisquam incidunt.</p>
+                </div>
+
+                <div class="pasos">
+                <h3>PASO 2:</h3>
+                <img src={imagen1} alt="" />
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, aliquid dolorem obcaecati itaque ad tenetur vero impedit accusantium aut modi atque eum quod, ducimus eos numquam error iusto, quisquam incidunt.</p>
+                </div>
+
+
+                <div class="pasos">
+                <h3>PASO 3:</h3>
+                <img src={imagen1} alt="" />
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, aliquid dolorem obcaecati itaque ad tenetur vero impedit accusantium aut modi atque eum quod, ducimus eos numquam error iusto, quisquam incidunt.</p>
+                </div>
+
+            </div>
+            
+    
+
+
             </div>
         );
     }
--- src/componentes/QuienesSomos/QuienesSomos.js
+++ src/componentes/QuienesSomos/QuienesSomos.js
@@ -1,11 +1,78 @@
 import React, { Component } from 'react';
 
+//Importamos la imagen de quienes somos
+import imagen from '../elementos/img/quienes-somos.jpg';
+import imagen1 from '../elementos/img/historia.jpg';
+import imagen2 from '../elementos/img/objetivo.jpg';
+import imagen3 from '../elementos/img/users.png';
+
 class QuienesSomos extends Component {
     render() { 
         return (
-            <div className="QuienesSomos">
-                <h1>Componente quienes somos</h1>
+            <div class="ComoComprar">
+                <div class="contenedor-info">
+                    <img src={imagen} alt="Imagen registro " />
+                    <div class="info">
+                        <h2>¿Quienes somos?</h2>
+                        <p>Descubre mas sobre nosotros y de nuestro trabajo</p>
+                    </div>
+                </div>
+            <div class="Nosotros">
+                <h1>Nuestra Historia</h1>
+                <img class="textura" src={imagen1}/>
+                <p>Un día la estimada Serpas citó a su amigo Javier para comentarle que quería abrir un restaurante, una de esas asignaturas pendientes. Para ello necesitaba un socio en el que confiase ciegamente y allí en un sofá de su casa, cerveza en mano, se empezó a forjar la idea. No tardaron en empezar a hablar de una de sus pasiones, la hamburguesa, e hicieron un recorrido por las mejores hamburgueserías de Chicago, NY, Londres, Berlín… y llegaron a una conclusión: no hay hamburgueserías que traten su producto desde un punto de vista estrictamente gourmet.
+                </p>
             </div>
+            <div class="Nosotros">
+                <h1>Nuestros Objetivos</h1>
+                <img class="textura" src={imagen2}/>
+                <div class="lista">
+                <ul>
+                   <li>Ofrecer la mejor atención a nuestros clientes de forma atenta y confiable.</li>
+                   <li>Darle importancia a cada uno de nuestros clientes pues de ellos depende nuestro éxito.</li>
+                   <li>Elaborar los platos gastronómicos llenos de sabores y sensaciones más placenteras para el paladar de nuestros clientes</li>
+                   <li>Crear programas de oferta donde podamos involucrar a nuestros clientes cautivos.</li>
+                </ul>
+                </div>
+            </div>
+
+            <div class="Nosotros">
+                <h1>Nuestro Equipo</h1>
+            <div class="rejilla">
+                <div>
+                    <img class="users" src={imagen3}/>
+                    <p>Manuel Hernandez </p>
+                </div>
+                <div>
+                    <img class="users" src={imagen3}/>
+                    <p>Rene Carrillo </p>
+                </div>
+                <div>
+                    <img class="users" src={imagen3}/>
+                    <p>Jaime Navarrete</p>
+                </div>
+                <div>
+                    <img class="users" src={imagen3}/>
+                    <p>Irvin Castro</p>
+                </div>
+                <div>
+                    <img class="users" src={imagen3}/>
+                    <p>Daniel Benavides</p>
+                </div>
+                <div>
+                    <img class="users" src={imagen3}/>
+                    <p>Cesar Ventura</p>
+                </div>
+                
+            </div>
+            </div>
+        </div>
+
+
+
+
+
+            
         );
     }
 }
