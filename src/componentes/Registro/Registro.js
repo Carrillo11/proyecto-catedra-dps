7e355e73a88fffd8903ec32930c3951990bacb9d
Jaime Navarrete <jaimenava123@outlook.com>
2020-10-21 20:34:20 -0600

    Modificado - Estilos de la pagina de registro

--- src/componentes/Registro/Registro.js
+++ src/componentes/Registro/Registro.js
@@ -4,6 +4,8 @@ import 'firebase/auth';
 
 import { Link } from 'react-router-dom';
 
+import imagen from './../elementos/img/img-registro.jpg';
+
 
 const Registro = ()=>{
     const [email, setEmail] =useState('');
@@ -16,16 +18,45 @@ const Registro = ()=>{
     }
     
         return (
-            <div className="InicioSesion">
-        <h1>Registro</h1>
-        <div className="form">
-            <div className="grupo-inputs">
-                <input type="email" id='email' placeholder="Email" onChange={ (ev)=> setEmail(ev.target.value)} />
-                <input type="password" placeholder="Contraseña" onChange={ (ev)=> setPassword(ev.target.value)}/>
+
+            <div className="Registro">
+                <div className="contenedor-info">
+                    <img src={imagen} alt="Imagen registro" />
+                    <div className="info">
+                        <h2>Registro</h2>
+                        <p>Registrate para poder comprar y tambien para estar al tanto de nuestras promociones.</p>
+                    </div>
+                </div>
+
+                <h1>Registro</h1>
+                <div className="form-cuenta">
+                    {/* <div className="grupo-inputs">
+                        <h3>Datos personales</h3>
+                        <input type="text" placeholder="Nombres *" />
+                        <input type="text" placeholder="Apellidos *" />
+                        <input type="text" placeholder="DUI *" />
+                        <input type="date" placeholder="Fecha de nacimiento" />
+                        <input type="text" placeholder="Número de teléfono *" />
+                    </div> */}
+                    <div className="grupo-inputs">
+                        <h3>Datos de usuario</h3>
+                        {/* <input type="text" placeholder="Nombre de usuario *" />
+                        <input type="email" placeholder="Correo electrónico *" />
+                        <input type="password" placeholder="Contraseña *" />
+                        <input type="password" placeholder="Repetir contraseña *" /> */}
+
+                        <input type="email" id='email' placeholder="Email" onChange={ (ev)=> setEmail(ev.target.value)} />
+                        <input type="password" placeholder="Contraseña" onChange={ (ev)=> setPassword(ev.target.value)}/>
+                    </div>
+                    {/* <input type="checkbox" id="aceptarTerminos" /> */}
+                    {/* <label htmlFor="aceptarTerminos">Acepto los terminos y condiciones</label> */}
+                    <button class="botonIniciarSesion" onClick ={submit}>Registrarse</button>
+                </div>
+                <div className="existencia-cuenta">
+                    <p>¿Ya tienes una cuenta?</p>
+                    <Link to="/inicioSesion"><button class="botonIrInicioSesion">Iniciar sesión</button></Link>
+                </div>
             </div>
-            <button class="botonIniciarSesion" onClick ={submit}>Registrate</button>
-        </div>
-    </div>
         );
 }
  
