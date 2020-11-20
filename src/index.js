9cb67a7dafd0a5b8cfd984648b6792320f8c19a5
Carrillo11 <renecarrillo120@gmail.com>
2020-11-20 06:31:58 -0600

    Se ha agregado contenido a index.css y index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootswatch/dist/litera/bootstrap.min.css';
+import 'bootswatch/dist/lumen/bootstrap.min.css';
 import { render } from '@testing-library/react';
 
 // import SplashScreen from './Componentes/elementos/SplashScreen/SplashScreen';
